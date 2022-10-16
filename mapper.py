5f8f75bbfc3bad9188271a6bf1f97fda728f0d98
foufei <jiayuki@outlook.com>
2022-10-15 21:08:11 -0400

    update mapper

--- mapper.py
+++ mapper.py
@@ -1,55 +1,35 @@
 # #!/usr/bin/env python
 # import sys, re
-# import random
-# import pandas as pd
-# import os
-#
-# # build valience lookup table
-# # valences_df = pd.read_csv('AFINN-en-165.txt', sep='\t', header=None, names = ["word", "valence"])
-# # valence_lookup_table = dict(zip(valences_df.word, valences_df.valence))
-#
-# def main(argv):
-#     word_count = 0
-#     valence_aggregate = 0
-#
-#     # input comes from STDIN (standard input)
-#     for line in sys.stdin:
-#         # remove leading and trailing whitespace
-#         line = line.strip()
-#         # split the line into words
-#         words = line.split()
-#         # increase counters
-#         for word in words:
-#             # write the results to STDOUT (standard output);
-#             # what we output here will be the input for the
-#             # Reduce step, i.e. the input for reducer.py
-#             #
-#             # tab-delimited; the trivial word count is 1
-#             # valence_aggregate += valence_lookup_table[word]
-#             # word_count += 1
-#             print(word + "\t" + "1")
-#
-#
-# if __name__ == "__main__":
-#     main(sys.argv)
-#
-#
+import re
+import pandas as pd
+import os
+import sys
 
-#!/usr/bin/env python
-import sys, re
-import random
+# build valience lookup table
+# valences_df = pd.read_csv('AFINN-en-165.txt', sep='\t', header=None, names = ["word", "valence"])
+# valence_lookup_table = dict(zip(valences_df.word, valences_df.valence))
 
 def main(argv):
-    line = sys.stdin.readline()
-    pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
-    try:
-        while line:
+    word_count = 0
+    valence_aggregate = 0
+
+    # input comes from STDIN (standard input)
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+        # keep alphabets
+        pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
+        # split the line into words
+        words = line.split()
+        try:
             for word in pattern.findall(line):
                 print ("LongValueSum:" + word.lower() + "\t" + "1")
-                # x = 1 / random.randint(0,99)
-            line = sys.stdin.readline()
-    except EOFError as error:
-        return None
+
+        except EOFError as error:
+            return None
+
+
 
 if __name__ == "__main__":
     main(sys.argv)
+
